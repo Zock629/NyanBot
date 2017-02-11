5eeb43cf1b3537b20e3bb9a3ce838c56bbfeed4b
0eragera09 <gg873702@gmail.com>
2017-02-11 16:17:18 +0530

    Let's see

--- NyanCogs/NyanVCTestPlay.py
+++ NyanCogs/NyanVCTestPlay.py
@@ -1,5 +1,4 @@
 import discord
-import asyncio
 import youtube_dl
 import os
 from discord.ext import commands
@@ -11,7 +10,6 @@ class NyanVCTestPlay:
 		discord.opus.load_opus("libopus.so.0")
 
 	@commands.command(name='youtube', pass_context=True)
-	@asyncio.coroutine
 	async def _youtube(self, ctx):
 		server = ctx.message.server
 		voicebot = client.voice_client_in(message.server)
@@ -23,18 +21,14 @@ class NyanVCTestPlay:
 		player.start()
 	
 	@commands.command(name='testplay', pass_context=True)
-	@asyncio.coroutine
 	async def _join_voice_channel(self, ctx):
 		server = ctx.message.server
 		voicebot = self.bot.voice_client_in(server)
 		channel = voicebot.channel
-		print(channel)
-		try:
-			await self.bot.join_voice_channel(channel)
-			player = voice.create_ffmpeg_player('cool.mp3')
-			player.start()
-		except:
-			pass
-		
+		#try:
+		player = voicebot.create_ffmpeg_player('cool.mp3')
+		player.start()
+		#except:
+			
 def setup(bot):
 	bot.add_cog(NyanVCTestPlay(bot))
